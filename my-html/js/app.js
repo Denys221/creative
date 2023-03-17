c1a913a1b121a75ddb343907081c7addd3298a7b
Den <Bossaaaaa83@gmail.com>
2023-03-17 09:47:22 +0100

    correction

--- my-html/js/app.js
+++ my-html/js/app.js
@@ -10,7 +10,6 @@ ScrollSmoother.create({   // создаем
    effects: true,         // щоб можна було обратится к любому блоку і дати йому свою скорость
 })
 */
-
 if (ScrollTrigger.isTouch !== 1) { // если ScrollTrigger.isTouch не ровно 1 виполняем всі остальні условия.
    // якщо будуть дивитися на таблеті чи телефоні то ці параметри не будуть застосовуватися
    ScrollSmoother.create({   // создаем
@@ -25,23 +24,26 @@ if (ScrollTrigger.isTouch !== 1) { // если ScrollTrigger.isTouch не ров
       scrollTrigger: {
          trigger: '.hero-section',
          start: 'center',
-         end: '820',
+         end: '950',
          scrub: true
       }
    })
 
-   gsap.fromTo('.gallery__left .gallery__item', { x: -500, opacity: 0 }, { // анимация .gallery__left .gallery__item' + items виїжджають злівої сторони
-      opacity: 1, x: 0,
-      scrollTrigger: {
-         trigger: '.gallery__item',
-         scrub: true
-      }
-   })
 
-   let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
 
-   itemsL.forEach(item => {
-      gsap.fromTo(item, { x: -100, opacity: 0 }, {
+   /* с начала робимо так 
+  gsap.fromTo('.gallery__left .gallery__item', { x: -400, opacity: 0 }, { // анимация .gallery__left .gallery__item' + items виїжджають злівої сторони
+     opacity: 1, x: 0,
+     scrollTrigger: {
+        trigger: '.gallery__item',
+        scrub: true
+     }
+  })
+  */
+   let itemsL = gsap.utils.toArray('.gallery__left .gallery__item') // переменна для items з лівої сторони ( let itemsL )
+
+   itemsL.forEach(item => {    // щоб items виїжджали з лівої сторони і по черзі а не всі разом 
+      gsap.fromTo(item, { x: -200, opacity: 0 }, {
          opacity: 1, x: 0,
          scrollTrigger: {
             trigger: item,
@@ -52,10 +54,10 @@ if (ScrollTrigger.isTouch !== 1) { // если ScrollTrigger.isTouch не ров
       })
    })
 
-   let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+   let itemsR = gsap.utils.toArray('.gallery__right .gallery__item') // переменна для items з правої сторони ( let itemsR )
 
-   itemsR.forEach(item => {
-      gsap.fromTo(item, { x: 100, opacity: 0 }, {
+   itemsR.forEach(item => {     // щоб items виїжджали з правої сторони і по черзі а не всі разом 
+      gsap.fromTo(item, { x: 350, opacity: 0 }, {
          opacity: 1, x: 0,
          scrollTrigger: {
             trigger: item,
